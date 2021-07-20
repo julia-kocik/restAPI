4ea1878a5c0f9621b049f77fbaeec9d3da194f93
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-07-20 11:59:49 +0200

    Add get

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require('express');
 
+const db = [
+    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+];
+
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/testimonials', (req, res) => {
+    res.json(db)
+});
+
+app.get('/testimonials/:id', (req, res) => {
+    res.json(db.find(elem => elem.id === req.params.id))
+});
+
+app.get('/testimonials/random', (req, res) => {
+    res.json(db[`${Math.floor(Math.random() * db.length)}`])
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+    res.json(db[id])
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
